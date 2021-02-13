db2296b70587d96420ac8ffc76828d0189ed0af6
Dmitry Butrimov <d.butrimov@gmail.com>
2021-02-14 00:13:11 +0300

    update envs

--- project/plaxt/settings/common.py
+++ project/plaxt/settings/common.py
@@ -7,5 +7,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Read environment variables
 ENV = environ.Env()
 
-env_file = ENV.str('ENV_FILE', default=os.path.join(BASE_DIR, '.env'))
-environ.Env.read_env(env_file=env_file)
+env_path = ENV.str('ENV_PATH', default=os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(env_file=env_path)
