777e0a9792c5f864214999173590a60b74a26674
Dmitry Butrimov <d.butrimov@gmail.com>
2022-08-04 22:44:13 +0300

    bump version

--- plaxt/plaxt/__init__.py
+++ plaxt/plaxt/__init__.py
@@ -2,7 +2,7 @@
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 VERSION = __version__
 
 __all__ = ('celery_app',)
